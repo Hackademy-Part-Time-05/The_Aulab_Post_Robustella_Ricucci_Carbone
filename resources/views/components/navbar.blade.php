4dcd02715fc4ffec7c0dc8feffcca444eda03f88
simoricu <simone@simonericucci.com>
2023-05-10 20:51:27 +0200

    Creazione Models Article, Category e User

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,16 +1,16 @@
 <nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-          </ul>
-        </div>
+      <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+            </ul>
+          </div>
       </div>
       @auth
       <li class="nav-item dropdown">
@@ -28,18 +28,18 @@
             @csrf
             </form>
         </ul>
+      </li>
+      @endauth
+      @guest
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Benvenuto Ospite</a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown"></ul>
         </li>
-        @endauth
-        @guest
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Benvenuto Ospite</a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown"></ul>
-          </li>
-        @endguest
-          <li>
-            <a class="dropdown-item" href="{{ route('register') }}">Registrati</a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="{{ route('login') }}">Accedi</a>
-          </li>
-    </nav>
+      @endguest
+        <li>
+          <a class="dropdown-item" href="{{ route('register') }}">Registrati</a>
+        </li>
+        <li>
+          <a class="dropdown-item" href="{{ route('login') }}">Accedi</a>
+        </li>
+</nav>
