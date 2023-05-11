ceb84342b227d53b07dea90800f9f3767764b986
Marco Robustella <marco.robustella98@gmail.com>
2023-05-11 21:41:07 +0200

    aggiunto article index

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ use App\Http\Controllers\ArticleController;
 Route::get('/', [PublicController::class, 'homepage'])->name('homepage');
 Route::get('/article/create', [ArticleController::class, 'create'])->name('article.create');
 Route::post('/article/store', [ArticleController::class, 'store'])->name('article.store');
+Route::get('/article/index', [ArticleController::class, 'index'])->name('article.index');
