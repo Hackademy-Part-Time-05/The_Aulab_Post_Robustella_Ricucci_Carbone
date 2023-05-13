3424a8be3212363fc04f2b96167112a7fe62255f
Marco Robustella <marco.robustella98@gmail.com>
2023-05-13 09:35:01 +0200

    aggiunta rotta per categorie e fixato article show

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -86,3 +86,4 @@ class ArticleController extends Controller
         //
     }
 }
+
--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -13,7 +13,7 @@
                 @foreach($articles as $article)
                 <div class="col-12 ocl-md-3 my-2">
                     <div class="card">
-                        <img src="{{Storage::url($articlse->image)}}" class="card-img-top" alt="...">
+                        <img src="{{Storage::url($article->image)}}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="class-title">{{$article->title}}</h5>
                             <p class="card-text"{{$article->subtitle}}</p>
